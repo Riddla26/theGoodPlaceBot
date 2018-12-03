4171d2a63ea4cc16feab34d97181009e8313bd13
Robert Schillinger <schillinger.rob@gmail.com>
2018-12-02 18:14:20 -0600

    Set up reddit wrapper and comment stream

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-"use strict";
+// includes
+const snoowrap = require('snoowrap');
+const snoostorm = require('snoostorm');
+
+// config
+const sub = 'theGoodPlace';
+
+const config = {
+  userAgent: 'GoodPlaceBot',
+  clientId: process.env.ID,
+  clientSecret: process.env.SECRET,
+  username: process.env.USERNAME,
+  password: process.env.PASS,
+};
+
+const r = new snoowrap(config);
+const client = new snoostorm(r);
+
+const commentStream = client.CommentStream({
+  subreddit: sub,
+  results: 20,
+  pollTime: 2000,
+});
+
+commentStream.on('comment', (comment) => {
+  console.log('Comment:');
+  console.log(comment);
+});
