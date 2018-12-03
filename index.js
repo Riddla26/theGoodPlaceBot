a935d867a962a61dfd665b32b8685592751db02f
Robert Schillinger <schillinger.rob@gmail.com>
2018-12-03 17:46:36 -0600

    add userSchema static to fetch top/bottom 10 users

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 // includes
-const snoowrap = require('snoowrap');
-const snoostorm = require('snoostorm');
-const Comment = require('./components/comment');
 const db = require('./components/database');
 const dbConfig = require('./config/database');
+
+const Comment = require('./components/comment');
 const User = require('./models/user');
+
+const snoowrap = require('snoowrap');
+const snoostorm = require('snoostorm');
+const async = require('async');
 
 // start up our db
 db.init(dbConfig.url);
 
 // reddit config
-const sub = 'theGoodPlace';
+const sub = 'politics';
 const config = {
   userAgent: 'GoodPlaceBot',
   clientId: process.env.ID,
@@ -31,7 +34,7 @@ const commentStream = client.CommentStream({
 
 commentStream.on('comment', (comment) => {
   const parser = new Comment(comment.body, comment.id);
-  
+
   parser.processComment()
     .then((data) => {
       User.findOrCreate({ username: comment.author.name }, (err, user) => {
@@ -45,3 +48,17 @@ commentStream.on('comment', (comment) => {
       });
     });
 });
+
+const fetchScoreboard = () => {
+  async.parallel({
+    highest: (callback) => {
+      User.getTen('desc', callback);
+    },
+    lowest: (callback) => {
+      User.getTen('asc', callback);
+    },
+  }, (err, scoreboard) => {
+    if (err) throw err;
+    console.log('>>> scoreboard', scoreboard);
+  });
+};
--- models/user.js
+++ models/user.js
@@ -10,12 +10,18 @@ const userSchema = new Schema({
 });
 
 userSchema.statics.findOrCreate = function findOrCreate(condition, callback) {
-  return new Promise((resolve) => {
-    const self = this;
-    self.findOne(condition, (err, result) => {
-      return result ? callback(err, result) : self.create(condition, (err, result) => { return callback(err, result) });
-    });
+  const self = this;
+  self.findOne(condition, (err, result) => {
+    return result ? callback(err, result) : self.create(condition, (err, result) => { return callback(err, result) });
   });
+};
+
+userSchema.statics.getTen = function getTen(direction, callback) {
+  const self = this;
+  self.find({})
+    .sort({ score: direction })
+    .limit(10)
+    .exec(callback);
 };
 
 module.exports = mongoose.model('User', userSchema);
